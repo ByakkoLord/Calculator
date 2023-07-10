4cfedd7d8bb00aa1aa61b7aa4f593e63f59b1000
ByakkoLord <g.fonsecadacosta@gmail.com>
2023-07-10 14:33:56 -0300

    Calculator

---
+++ src/main.js
@@ -0,0 +1,26 @@
+var btn_C = document.querySelector('#btn_C')
+var btn_CE = document.querySelector('#btn_CE')
+var btn_div = document.querySelector('#btn_div')
+var btn_mult = document.querySelector('#btn_mult')
+var btn_1 = document.querySelector('#btn_1')
+var btn_2 = document.querySelector('#btn_2')
+var btn_3 = document.querySelector('#btn_3')
+var btn_min = document.querySelector('#btn_min')
+var btn_4 = document.querySelector('#btn_4')
+var btn_5 = document.querySelector('#btn_5')
+var btn_6 = document.querySelector('#btn_6')
+var btn_plus = document.querySelector('#btn_plus')
+var btn_7 = document.querySelector('#btn_7')
+var btn_8 = document.querySelector('#btn_8')
+var btn_9 = document.querySelector('#btn_9')
+var btn_equal = document.querySelector('#btn_equal')
+var btn_0 = document.querySelector('#btn_0')
+var btn_dot = document.querySelector('#btn_dot')
+
+function addNumber(event){
+    var number = event.target
+}
+
+btn_C.addEventListener('click', function addNumber (){
+
+})
